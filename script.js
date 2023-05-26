f4f408820657986182441dc8b6005eb2b309c293
srimythri1018 <115878123+srimythri1018@users.noreply.github.com>
2023-05-26 07:55:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // complete the given function
 
 function palindrome(str){
+	const arr=[];
+	var ans=true;
+	let n=str.lenght;
+	for(let i=0;i<n;i++){
+		arr.push(str.charAt[i]);
+	}
+	for(let i=0;i<n/2;i++)
+		{
+			for(let j=n-1;j>n/2;j--){
+			if(arr[i]!=arr[j]){
+				ans=false;
+			}
+			}
+		}
+	console.log(ans);
 
 }
 module.exports = palindrome
