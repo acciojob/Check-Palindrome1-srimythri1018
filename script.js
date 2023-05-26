c56c9cc4e127a0a920c168099df94b0b40068011
srimythri1018 <115878123+srimythri1018@users.noreply.github.com>
2023-05-26 08:00:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 // complete the given function
 
-function palindrome(str){
-	const arr=[];
-	var ans=true;
-	let n=str.lenght;
-	for(let i=0;i<n;i++){
-		arr.push(str.charAt[i]);
-	}
-	for(let i=0;i<n/2;i++)
-		{
-			for(let j=n-1;j>n/2;j--){
-			if(arr[i]!=arr[j]){
-				ans=false;
-			}
-			}
-		}
-	console.log(ans);
-
+function isPalindrome(s) {
+  s = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+  return s === s.split('').reverse().join('');
 }
+
+// Example usage:
+const input = "race a car";
+const result = isPalindrome(input);
+console.log(result);
+
 module.exports = palindrome
